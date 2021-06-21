b97af888cb0d120e94fb833f21223e8c96804061
jamsonkuruma <85690562+jamsonkuruma@users.noreply.github.com>
2021-06-21 16:32:57 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,28 +7,22 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def karwan(_, message: Message):
     await message.reply_text(
-        f"""I am an nezoku 👇this is my developers👇
+        f""" 🙃🙃🙃 """,
 
-join my developer gruops and shapport me :- @SL_MEDIA_TECH_GRUOP , @NovaTechlk
-My developers
 
-🌟AKILA :-  @Akilawiduruwan
-🌟PAHAN :- @Humangasor
-my music play command 👇
-/mute - 🔇 Mute the user bot
-/stop - 🔊 clear play list
-/play :- play a song
-        """,
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        "Group", url="https://t.me/SL_MEDIA_TECH_GRUOP"
-                    ),
-                    InlineKeyboardButton(
-                        "Channel", url="https://t.me/sl_media_tech"
-                    )
-                ]
-            ]
-        )
-    )
+
+
+
+        
+            
+                
+                   
+                        
+                    
+                    
+                        
+                    
+                
+            
+        
+    
